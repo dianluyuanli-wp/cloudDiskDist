fb81281f45f15267dd9ede9fc1d9479055d58d36
xiaohuli <waterdoor@foxmail.com>
2020-10-21 11:22:33 +0800

    new version

--- tag.js
+++ tag.js
@@ -11,12 +11,24 @@ function cb(error, stdout, stderr) {
   }
 }
 
-process.exec(`git add .`,cb);
-process.exec(`git commit -am "new version"`,cb);
-
-//  打tag
-process.exec(`git tag -a ${newVersion} -m "new version: ${newVersion}"`,cb);
-
-process.exec(`git push origin master`,cb);
-//  推tag
-process.exec(`git push origin ${newVersion}`,cb);
+function shellWithPromise(cmd) {
+  return new Promise(res => {
+    process.exec(cmd, (error, stdout, stderr) => {
+      cb(error, stdout, stderr);
+      res();
+    })
+  })
+}
+async function run() {
+  //  添加文件
+  await shellWithPromise(`git add .`);
+  //  打commit
+  await shellWithPromise(`git commit -am "new version"`);
+  //  打tag
+  await shellWithPromise(`git tag -a ${newVersion} -m "new version: ${newVersion}"`);
+  //  推到master
+  await shellWithPromise(`git push origin master`);
+  //  推tag
+  await shellWithPromise(`git push origin ${newVersion}`);
+}
+run();
